587681540e38e85ac48e293d77e338f8b74f49e4
zacks223 <zacksfung@gmail.com>
2019-06-11 20:21:13 +0800

    feat: Find All Numbers Disappeared in an Array(O(2n) Solution)

--- 448. Find All Numbers Disappeared in an Array/answer.js
+++ 448. Find All Numbers Disappeared in an Array/answer.js
@@ -1,4 +1,5 @@
 /**
+ * 时间复杂度O(n)
  * @param {number[]} nums
  * @return {number[]}
  */
@@ -15,4 +16,25 @@ var findDisappearedNumbers = function(nums) {
         delete keyStore[n];
     });
     return Object.keys(keyStore);
+};
+
+/**
+ * 时间复杂度O(2n)
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    let ret = new Array(nums.length);
+    let counter = 0;
+    nums.forEach(n => {
+        ret[n - 1] = n;
+    });
+    
+    for (let i = 0; i < ret.length; i++) {
+        if (ret[i] !== i + 1) {
+            counter++;
+            nums.push(i + 1);
+        }
+    };
+    return nums.splice(nums.length - counter, counter);
 };
